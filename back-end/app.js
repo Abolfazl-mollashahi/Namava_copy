8b4c94eeff625dbf27926ac2a48edaf727bc7d71
Mehran Khodaei <khodaei.dev@gmail.com>
2024-05-28 22:44:43 +0330

    Set Some middlewares in app.js

--- back-end/app.js
+++ back-end/app.js
@@ -1,14 +1,32 @@
 const express = require("express");
 const app = express();
-const cors = require("cors");
 const path = require("path");
-const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+const flash = require("express-flash");
 const authRoter = require("./moduls/auth/auth.routes");
+const { setHeaders } = require("./middleware/setHeader");
 
-app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+app.use(express.json({ limit: "50mb" }));
 
+// cookie Parser
+app.use(cookieParser());
+
+//express Flash and Sessions
+app.use(
+  session({
+    secret: process.env.Secret_Sessions,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+app.use(flash());
+
+// Set Header (cors Policy)
+app.use(setHeaders);
+
+// Routes
 app.use("/auth", authRoter);
 
 module.exports = app;
